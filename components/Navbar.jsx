1ec77586afe7e2c587873fb4db6d32588b30973d
phminh3033 <phminh3033@gmail.com>
2022-04-03 15:32:46 +0700

    end properties and begin nav & footer

---
+++ components/Navbar.jsx
@@ -0,0 +1,7 @@
+import { Link } from 'next/link';
+import { Menu, MenuButton, MenuList, MenuItem, IconButton, Flex, Box, Spacer } from '@chakra-ui/react';
+import {FcMenu, FcHome, FcAbout } from 'react-icons/fc';
+import { BsSearch } from 'react-icons/bs';
+import { FiKey } from 'react-icons/fi';
+
+const
